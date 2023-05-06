c5918738d405289c688f4f044cdc63ae51ac5ef2
MUSKAN <muskangoyal3477@gmail.com>
2023-05-07 00:06:23 +0530

    error handling

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+
+    'daphne',
+
     'django.contrib.staticfiles',
 
     'chat.apps.ChatConfig',
